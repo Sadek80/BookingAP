42108e90742232726749f3e6391d269e01bffebd
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 15:58:06 +0300

    Adds IsOverlappingAsync to IBookingRepository

--- BookingAP.Domain/Bookings/Repositories/IBookingRepository.cs
+++ BookingAP.Domain/Bookings/Repositories/IBookingRepository.cs
@@ -1,8 +1,13 @@
 ﻿using BookingAP.Domain.Abstractions;
+using BookingAP.Domain.Appartments;
+using BookingAP.Domain.Bookings.ValueObjects;
 
 namespace BookingAP.Domain.Bookings.Repositories
 {
     public interface IBookingRepository : IRepository<Booking>
     {
+        Task<bool> IsOverlappingAsync(Appartment apartment,
+                                      DateRange duration,
+                                      CancellationToken cancellationToken = default);
     }
 }
