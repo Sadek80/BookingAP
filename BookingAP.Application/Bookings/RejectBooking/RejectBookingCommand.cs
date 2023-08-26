a9398fd133bbb861871b2aef155b5df74e0f4b43
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 18:11:05 +0300

    Implement RejectBooking Command

---
+++ BookingAP.Application/Bookings/RejectBooking/RejectBookingCommand.cs
@@ -0,0 +1,6 @@
+﻿using BookingAP.Application.Abstractions.Messaging;
+using ErrorOr;
+
+namespace Bookify.Application.Bookings.RejectBooking;
+
+public sealed record RejectBookingCommand(Guid BookingId) : ICommand<ErrorOr<bool>>;
---
+++ BookingAP.Application/Bookings/RejectBooking/RejectBookingCommandCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using BookingAP.Application.Abstractions.Clock;
+using BookingAP.Application.Abstractions.Messaging;
+using BookingAP.Application.Abstractions.Repositories;
+using BookingAP.Application.Abstractions.Repositories.Bookings;
+using BookingAP.Domain.Abstractions;
+using ErrorOr;
+using static BookingAP.Domain.Bookings.DomainErrors;
+
+namespace Bookify.Application.Bookings.RejectBooking;
+
+internal sealed class RejectBookingCommandCommandHandler : ICommandHandler<RejectBookingCommand, ErrorOr<bool>>
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RejectBookingCommandCommandHandler(
+        IDateTimeProvider dateTimeProvider,
+        IBookingRepository bookingRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _bookingRepository = bookingRepository;
+        _unitOfWork = unitOfWork;
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    public async Task<ErrorOr<bool>> Handle(RejectBookingCommand request, CancellationToken cancellationToken)
+    {
+        var booking = await _bookingRepository.GetByIdAsync(request.BookingId, cancellationToken);
+
+        if (booking is null)
+        {
+            return DomainError.NotFound(BookingErrors.NotFound);
+        }
+
+        var result = booking.Reject(_dateTimeProvider.UTCNow);
+
+        if (result.IsError)
+        {
+            return result;
+        }
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return result;
+    }
+}
