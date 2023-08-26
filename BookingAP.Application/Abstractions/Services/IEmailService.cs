32fd1f47a26c9bd5f2520dd5ee9f3d9779b513cc
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 15:57:39 +0300

    Adds IEmailService abstraction

---
+++ BookingAP.Application/Abstractions/Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using BookingAP.Domain.Users.ValueObjects;
+
+namespace BookingAP.Application.Abstractions.Services
+{
+    public interface IEmailService
+    {
+        Task SendAsync(Email email, string subject, string body);
+    }
+}
