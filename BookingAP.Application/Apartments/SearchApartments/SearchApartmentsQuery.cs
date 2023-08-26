35a18f5c559780e99f854088c72a469c8f7b9b5d
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 19:39:10 +0300

    Implements Appartment Features

---
+++ BookingAP.Application/Apartments/SearchApartments/SearchApartmentsQuery.cs
@@ -0,0 +1,7 @@
+﻿using BookingAP.Application.Abstractions.Messaging;
+using ErrorOr;
+
+namespace Bookify.Application.Apartments.SearchApartments;
+
+public sealed record SearchApartmentsQuery(DateOnly StartDate,
+                                           DateOnly EndDate) : IQuery<ErrorOr<IReadOnlyList<ApartmentResponse>>>;
---
+++ BookingAP.Application/Apartments/SearchApartments/SearchApartmentsQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using BookingAP.Application.Abstractions.Messaging;
+using BookingAP.Application.Abstractions.Repositories.Appartments;
+using BookingAP.Domain.Bookings.Enums;
+using ErrorOr;
+
+namespace Bookify.Application.Apartments.SearchApartments;
+
+internal sealed class SearchApartmentsQueryHandler
+    : IQueryHandler<SearchApartmentsQuery, ErrorOr<IReadOnlyList<ApartmentResponse>>>
+{
+    private static readonly int[] ActiveBookingStatuses =
+    {
+        (int)BookingStatus.Reserved,
+        (int)BookingStatus.Confirmed,
+        (int)BookingStatus.Completed
+    };
+
+    private readonly IAppartmentRepository _appartmentRepository;
+
+    public SearchApartmentsQueryHandler(IAppartmentRepository appartmentRepository)
+    {
+        _appartmentRepository = appartmentRepository;
+    }
+
+    public async Task<ErrorOr<IReadOnlyList<ApartmentResponse>>> Handle(SearchApartmentsQuery request, CancellationToken cancellationToken)
+    {
+        if (request.StartDate > request.EndDate)
+        {
+            return new List<ApartmentResponse>();
+        }
+
+        var appartments = await _appartmentRepository.Search<ApartmentResponse>(request.StartDate, request.StartDate, ActiveBookingStatuses, cancellationToken);
+
+        return appartments.ToList();
+    }
+}
