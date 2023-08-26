a41938f56b8683ca51399cbd877270c3232e0daf
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 18:10:05 +0300

    Implements GetBooking Feature

---
+++ BookingAP.Application/Bookings/GetBooking/GetBookingQuery.cs
@@ -0,0 +1,6 @@
+﻿using BookingAP.Application.Abstractions.Messaging;
+using ErrorOr;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+public sealed record GetBookingQuery(Guid BookingId) : IQuery<ErrorOr<BookingResponse>>;
---
+++ BookingAP.Application/Bookings/GetBooking/GetBookingQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using BookingAP.Application.Abstractions.Messaging;
+using BookingAP.Application.Abstractions.Repositories.Bookings;
+using ErrorOr;
+
+namespace Bookify.Application.Bookings.GetBooking;
+
+internal sealed class GetBookingQueryHandler : IQueryHandler<GetBookingQuery, ErrorOr<BookingResponse>>
+{
+    private readonly IBookingRepository _bookingRepository;
+
+    public GetBookingQueryHandler(IBookingRepository bookingRepository)
+    {
+        _bookingRepository = bookingRepository;
+    }
+
+    public async Task<ErrorOr<BookingResponse>> Handle(GetBookingQuery request, CancellationToken cancellationToken)
+    {
+        var booking = await _bookingRepository.GetBookingDetails<BookingResponse>(request.BookingId, cancellationToken);
+
+        return booking;
+    }
+}
