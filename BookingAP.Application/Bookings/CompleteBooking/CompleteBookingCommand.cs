af43603ff4ed4b01d89bbe280295d4a20b409522
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 18:10:38 +0300

    Implements CompleteBooking Feature

---
+++ BookingAP.Application/Bookings/CompleteBooking/CompleteBookingCommand.cs
@@ -0,0 +1,6 @@
+﻿using BookingAP.Application.Abstractions.Messaging;
+using ErrorOr;
+
+namespace Bookify.Application.Bookings.CompleteBooking;
+
+public record CompleteBookingCommand(Guid BookingId) : ICommand<ErrorOr<bool>>;
