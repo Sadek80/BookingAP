3920f90543e68b8f2736e970625135f0dda0609a
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 15:15:14 +0300

    Register ValidationPipelineBehavior and BookingPricingService

--- BookingAP.Application/DependencyInjection.cs
+++ BookingAP.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using BookingAP.Application.Abstractions.Behaviors;
+using BookingAP.Domain.Bookings.Services;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BookingAP.Application
@@ -12,7 +14,13 @@ namespace BookingAP.Application
                 configuration.RegisterServicesFromAssemblies(typeof(DependencyInjection).Assembly);
 
                 configuration.AddOpenBehavior(typeof(LoggingPipelineBehavior<,>));
+
+                configuration.AddOpenBehavior(typeof(ValidationPipelineBehavior<,>));
             });
+
+            services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
+
+            services.AddTransient<BookingPricingService>();
 
             return services;
         }
