f84d78888204170d1469eead3f8b28221dfe84b3
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 15:15:02 +0300

    Adds ValidationPipelineBehavior

---
+++ BookingAP.Application/Abstractions/Behaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,46 @@
+﻿using BookingAP.Application.Abstractions.Messaging;
+using ErrorOr;
+using FluentValidation;
+using MediatR;
+
+namespace BookingAP.Application.Abstractions.Behaviors
+{
+    public class ValidationPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : ICommand<TResponse>
+    where TResponse : IErrorOr
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationPipelineBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (!_validators.Any())
+            {
+                return await next();
+            }
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationErrors = _validators
+                .Select(validator => validator.Validate(context))
+                .Where(validationResult => validationResult.Errors.Any())
+                .SelectMany(validationResult => validationResult.Errors)
+                .ToList()
+                .ConvertAll(validationFailure =>
+                            Error.Validation(validationFailure.PropertyName,
+                                             validationFailure.ErrorMessage));
+
+            if(validationErrors is null || !validationErrors.Any())
+            {
+                return await next();
+            }
+
+
+            return (dynamic)validationErrors;
+        }
+    }
+}
