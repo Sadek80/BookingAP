c2609c3fc79951d0ff2019254d969a8c2d9a3f59
Sadek80 <mohamedsadk80@gmail.com>
2023-08-26 19:38:50 +0300

    Adds Search to IAppartmentRepository

--- BookingAP.Application/Abstractions/Repositories/Appartments/IAppartmentRepository.cs
+++ BookingAP.Application/Abstractions/Repositories/Appartments/IAppartmentRepository.cs
@@ -4,5 +4,6 @@ namespace BookingAP.Application.Abstractions.Repositories.Appartments
 {
     public interface IAppartmentRepository : IRepository<Appartment>
     {
+        Task<IReadOnlyList<TResult>> Search<TResult>(DateOnly startDate, DateOnly endDate, int[] activeBookingStatuses, CancellationToken cancellationToken = default);
     }
 }
