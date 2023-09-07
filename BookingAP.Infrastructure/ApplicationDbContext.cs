649ddb8b694958ac9d2e8c0d65595aa819152b69
Sadek80 <mohamedsadk80@gmail.com>
2023-09-07 15:59:40 +0300

    Adds MySql.EntityFrameworkCore and ApplicationDbContext

---
+++ BookingAP.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BookingAP.Application.Abstractions.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingAP.Infrastructure
+{
+    public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- BookingAP.Infrastructure/DependencyInjection.cs
+++ BookingAP.Infrastructure/DependencyInjection.cs
@@ -2,8 +2,10 @@
 using BookingAP.Application.Abstractions.Services;
 using BookingAP.Infrastructure.Clock;
 using BookingAP.Infrastructure.Services;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MySql.EntityFrameworkCore.Extensions;
 
 namespace BookingAP.Infrastructure
 {
@@ -13,6 +15,24 @@ namespace BookingAP.Infrastructure
         {
             services.AddTransient<IDateTimeProvider, DateTimeProvider>();
             services.AddTransient<IEmailService, EmailService>();
+
+            var connectionString = configuration.GetConnectionString("Database") ?? 
+                                   throw new ArgumentNullException("Database Connection String is Null");
+
+            services.AddEntityFrameworkMySQL();
+
+            services.AddDbContextPool<ApplicationDbContext>((serviceProvider, options) =>
+            {
+                options.UseMySQL(connectionString,
+                                 mySqlOptionsAction: sqlOptions =>
+                                 {
+                                     sqlOptions.EnableRetryOnFailure(maxRetryCount: 10,
+                                                                     maxRetryDelay: TimeSpan.FromSeconds(30),
+                                                                     errorNumbersToAdd: null);
+                                 });
+
+                options.UseInternalServiceProvider(serviceProvider);
+            });
 
             return services;
         }
