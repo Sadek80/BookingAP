6ac19392f98132800ca012ff2ac98abb6c64c2bb
Sadek80 <mohamedsadk80@gmail.com>
2023-09-07 19:31:33 +0300

    Adds Implementation For GenericRepo and BookingRepo

---
+++ BookingAP.Infrastructure/Repositories/Bookings/BookingRepository.cs
@@ -0,0 +1,32 @@
+﻿using BookingAP.Application.Abstractions.Repositories.Bookings;
+using BookingAP.Domain.Appartments;
+using BookingAP.Domain.Bookings;
+using BookingAP.Domain.Bookings.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingAP.Infrastructure.Repositories.Bookings
+{
+    internal sealed class BookingRepository : Repository<Booking>, IBookingRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public BookingRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task<TResult> GetBookingDetails<TResult>(Guid bookingId, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> IsOverlappingAsync(Appartment apartment, DateRange duration, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BookingAP.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,33 @@
+﻿using BookingAP.Application.Abstractions.Repositories;
+using BookingAP.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingAP.Infrastructure.Repositories
+{
+    internal class Repository<T> : IRepository<T> where T : Entity
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+        }
+
+        public async Task<T> AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            var entityAdded = await _dbContext.Set<T>().AddAsync(entity, cancellationToken);
+
+            return entityAdded.Entity;
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _dbContext.Set<T>().FirstOrDefaultAsync(f => f.Id == id, cancellationToken);
+        }
+    }
+}
