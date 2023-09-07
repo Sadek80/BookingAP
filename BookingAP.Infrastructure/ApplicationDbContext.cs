b45e8a959bed961a8f54666d3db11a91c91ccc54
Sadek80 <mohamedsadk80@gmail.com>
2023-09-07 19:20:19 +0300

    Adds Entity Configurations

--- BookingAP.Infrastructure/ApplicationDbContext.cs
+++ BookingAP.Infrastructure/ApplicationDbContext.cs
@@ -8,5 +8,12 @@ namespace BookingAP.Infrastructure
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ BookingAP.Infrastructure/Configurations/UserConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using BookingAP.Domain.Users;
+using BookingAP.Domain.Users.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookingAP.Infrastructure.Configurations
+{
+    internal sealed class UserConfigurations : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(nameof(User));
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(user => user.FirstName)
+                .HasMaxLength(200)
+                .HasConversion(firstName => firstName.Value, value => new FirstName(value));
+
+            builder.Property(user => user.LastName)
+                .HasMaxLength(200)
+                .HasConversion(firstName => firstName.Value, value => new LastName(value));
+
+            builder.Property(user => user.Email)
+                .HasMaxLength(400)
+                .HasConversion(email => email.Value, value => new Email(value));
+
+            builder.HasIndex(user => user.Email).IsUnique();
+        }
+    }
+}
