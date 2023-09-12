75e914f63b9b6ad0b3c3e1cf9f5276f677e995b2
Sadek80 <mohamedsadk80@gmail.com>
2023-09-12 15:39:27 +0300

    Adds EntityTypes Configurations

--- BookingAP.Infrastructure/ApplicationDbContext.cs
+++ BookingAP.Infrastructure/ApplicationDbContext.cs
@@ -9,5 +9,12 @@ namespace BookingAP.Infrastructure
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ BookingAP.Infrastructure/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using BookingAP.Domain.Appartments;
+using BookingAP.Domain.Bookings;
+using BookingAP.Domain.Reviews;
+using BookingAP.Domain.Reviews.ValueObjects;
+using BookingAP.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+
+internal sealed class ReviewConfiguration : IEntityTypeConfiguration<Review>
+{
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.ToTable("reviews");
+
+        builder.HasKey(review => review.Id);
+
+        builder.Property(review => review.Rating)
+            .HasConversion(rating => rating.Value, value => Rating.Create(value).Value);
+
+        builder.Property(review => review.Comment)
+            .HasMaxLength(200)
+            .HasConversion(comment => comment.Value, value => new Comment(value));
+
+        builder.HasOne<Appartment>()
+            .WithMany()
+            .HasForeignKey(review => review.Id);
+
+        builder.HasOne<Booking>()
+            .WithMany()
+            .HasForeignKey(review => review.Id);
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(review => review.Id);
+    }
+}
