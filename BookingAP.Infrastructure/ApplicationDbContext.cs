4cafb095545b8c0db9ca37d3e485885bcb6b31a6
Sadek80 <mohamedsadk80@gmail.com>
2023-09-12 15:28:12 +0300

    Adds DbContext to the infrastructure

---
+++ BookingAP.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using BookingAP.Application.Abstractions.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingAP.Infrastructure
+{
+    internal sealed class ApplicationDbContext : DbContext, IUnitOfWork
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ BookingAP.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BookingAP.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContextPool<ApplicationDbContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException(nameof(configuration));
+
+                options.UseMySQL(connectionString);
+            });
+
+            return services;
+        }
+    }
+}
