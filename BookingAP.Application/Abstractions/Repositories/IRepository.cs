2427f184c58f93b430a5841a50a441ffc3d4fe08
Sadek80 <mohamedsadk80@gmail.com>
2023-09-12 16:21:59 +0300

    Adds Implementation to IRepository

--- BookingAP.Application/Abstractions/Repositories/IRepository.cs
+++ BookingAP.Application/Abstractions/Repositories/IRepository.cs
@@ -4,8 +4,8 @@ namespace BookingAP.Application.Abstractions.Repositories
 {
     public interface IRepository<T> where T : Entity
     {
-        Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-        void Add(T entity, CancellationToken cancellationToken = default);
+        Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        void Add(T entity);
         Task<T> AddAsync(T entity, CancellationToken cancellationToken = default);
     }
 }
---
+++ BookingAP.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,32 @@
+﻿using BookingAP.Application.Abstractions.Repositories;
+using BookingAP.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingAP.Infrastructure.Repositories
+{
+    internal sealed class Repository<T> : IRepository<T> where T : Entity
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Add(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+        }
+
+        public async Task<T> AddAsync(T entity, CancellationToken cancellationToken = default)
+        {
+            await _dbContext.Set<T>().AddAsync(entity, cancellationToken);
+            return entity;
+        }
+
+        public async Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await  _dbContext.Set<T>().FirstOrDefaultAsync(f => f.Id == id);
+        }
+    }
+}
