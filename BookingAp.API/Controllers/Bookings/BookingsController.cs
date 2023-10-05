22f28f8428b12c227d2c450d5e540fe95c486c2e
Sadek80 <mohamedsadk80@gmail.com>
2023-10-05 16:28:09 +0300

    Adds Project Controllers

---
+++ BookingAp.API/Controllers/Bookings/BookingsController.cs
@@ -0,0 +1,50 @@
+﻿using BookingAP.Application.Bookings.GetBooking;
+using BookingAP.Application.Bookings.ReserveBooking;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingAp.API.Controllers.Bookings;
+
+[ApiController]
+[Route("api/bookings")]
+public class BookingsController : ApiController
+{
+    private readonly ISender _sender;
+
+    public BookingsController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetBooking(Guid id, CancellationToken cancellationToken)
+    {
+        var query = new GetBookingQuery(id);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return result.Match(success => Ok(result.Value),
+                            Problem);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> ReserveBooking(
+        ReserveBookingRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new ReserveBookingCommand(
+            request.ApartmentId,
+            request.UserId,
+            request.StartDate,
+            request.EndDate);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsError)
+        {
+            return Problem(result.Errors);
+        }
+
+        return CreatedAtAction(nameof(GetBooking), new { id = result.Value }, result.Value);
+    }
+}
---
+++ BookingAp.API/Controllers/Reviews/AddReviewRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace BookingAp.API.Controllers.Reviews;
+
+public sealed record AddReviewRequest(Guid BookingId, int Rating, string Comment);
