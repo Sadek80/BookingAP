68de803673dccb1c5c779a1058e799a9aab6c181
Sadek80 <mohamedsadk80@gmail.com>
2023-10-05 12:04:50 +0300

    Handles DomainEvents by Outbox Pattern

--- BookingAP.Infrastructure/ApplicationDbContext.cs
+++ BookingAP.Infrastructure/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
-﻿using BookingAP.Domain.Abstractions;
+﻿using BookingAP.Application.Abstractions.Scheduling;
+using BookingAP.Domain.Abstractions;
 using BookingAP.Domain.Exceptions;
+using BookingAP.Infrastructure.Scheduling;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
 
 namespace BookingAP.Infrastructure
 {
     public sealed class ApplicationDbContext : DbContext, IUnitOfWork
     {
+        private readonly IBackgroundJobService _backgroundService;
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
+            _backgroundService = this.GetInfrastructure().GetRequiredService<IBackgroundJobService>();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -17,16 +25,38 @@ namespace BookingAP.Infrastructure
             base.OnModelCreating(modelBuilder);
         }
 
-        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
             try
             {
-                return base.SaveChangesAsync(cancellationToken);
+                var result = await base.SaveChangesAsync(cancellationToken);
+
+                AddDomainEventsAsOutboxMessages();
+
+                return result;
             }
             catch (DbUpdateConcurrencyException ex)
             {
                 throw new ConcurrencyException("Try Again Later.", ex);
             }
+        }
+
+        private void AddDomainEventsAsOutboxMessages()
+        {
+            var domainEvents = ChangeTracker
+                .Entries<IEntity>()
+                .Select(entry => entry.Entity)
+                .SelectMany(entity =>
+                {
+                    var domainEvents = entity.GetDomainEvents();
+
+                    entity.ClearDomainEvents();
+
+                    return domainEvents;
+                })
+                .AsEnumerable();
+
+            _backgroundService.Enqueue<ProcessCoreEventJob>(e => e.Process(domainEvents));
         }
     }
 }
