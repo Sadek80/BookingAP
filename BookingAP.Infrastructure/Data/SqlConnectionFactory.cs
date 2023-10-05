98f98e8e9efa51626f7f5f6528b3e262f99ab748
Sadek80 <mohamedsadk80@gmail.com>
2023-10-05 10:37:51 +0300

    Adds SqlConnectionFactory Implementation and DateOnlyTypeHandler

---
+++ BookingAP.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,24 @@
+﻿using BookingAP.Application.Abstractions.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace BookingAP.Infrastructure.Data
+{
+    internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            var connection = new MySqlConnection(_connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
