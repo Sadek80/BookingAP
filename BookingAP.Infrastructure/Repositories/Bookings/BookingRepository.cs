790a6c33dd61455cefdbd8584a422a423f8d9860
Sadek80 <mohamedsadk80@gmail.com>
2023-11-05 16:24:25 +0200

    refactors Merging BookingRepo

--- BookingAP.Infrastructure/Repositories/Bookings/BookingRepository.cs
+++ BookingAP.Infrastructure/Repositories/Bookings/BookingRepository.cs
@@ -1,27 +1,52 @@
-﻿using BookingAP.Domain.Appartments;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using BookingAP.Domain.Appartments;
 using BookingAP.Domain.Bookings;
+using BookingAP.Domain.Bookings.Enums;
 using BookingAP.Domain.Bookings.Repositories;
 using BookingAP.Domain.Bookings.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookingAP.Infrastructure.Repositories.Bookings
 {
     internal sealed class BookingRepository : Repository<Booking>, IBookingRepository
     {
         private readonly ApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
 
-        public BookingRepository(ApplicationDbContext dbContext) : base(dbContext)
+        private static readonly BookingStatus[] ActiveBookingStatuses =
+        {
+        BookingStatus.Reserved,
+        BookingStatus.Confirmed,
+        BookingStatus.Completed
+        };
+
+
+        public BookingRepository(ApplicationDbContext dbContext, IMapper mapper) : base(dbContext)
         {
             _dbContext = dbContext;
+            _mapper = mapper;
         }
 
-        public Task<TResult> GetBookingDetails<TResult>(Guid bookingId, CancellationToken cancellationToken = default)
+        public async Task<TResult?> GetBookingDetails<TResult>(Guid bookingId, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Set<Booking>()
+                                   .AsNoTracking()
+                                   .ProjectTo<TResult>(_mapper.ConfigurationProvider)
+                                   .FirstOrDefaultAsync(cancellationToken);
         }
 
-        public Task<bool> IsOverlappingAsync(Appartment apartment, DateRange duration, CancellationToken cancellationToken = default)
+        public async Task<bool> IsOverlappingAsync(Appartment apartment, DateRange duration, CancellationToken cancellationToken = default)
         {
-            throw new NotImplementedException();
+            return await _dbContext
+                      .Set<Booking>()
+                      .AnyAsync(
+                          booking =>
+                              booking.Id == apartment.Id &&
+                              booking.Duration.Start <= duration.End &&
+                              booking.Duration.End >= duration.Start &&
+                              ActiveBookingStatuses.Contains(booking.Status),
+                              cancellationToken);
         }
     }
 }
