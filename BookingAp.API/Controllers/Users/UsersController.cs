2b7d130fbace1001bebb0e7f39402e1f58f0eac5
Sadek80 <mohamedsadk80@gmail.com>
2023-11-06 22:01:28 +0200

    Adds Users Controller

---
+++ BookingAp.API/Controllers/Users/UsersController.cs
@@ -0,0 +1,72 @@
+﻿using BookingAp.Contract.Users;
+using BookingAP.Application.Users.GetLoggedInUser;
+using BookingAP.Application.Users.LogInUser;
+using BookingAP.Application.Users.RegisterUser;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingAp.API.Controllers.Users;
+
+[Authorize]
+[ApiController]
+[Route("api/users")]
+public class UsersController : ApiController
+{
+    private readonly ISender _sender;
+
+    public UsersController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpGet("me")]
+    public async Task<IActionResult> GetLoggedInUser(CancellationToken cancellationToken)
+    {
+        var query = new GetLoggedInUserQuery();
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return Ok(result.Value);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(
+        RegisterUserRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new RegisterUserCommand(
+            request.Email,
+            request.FirstName,
+            request.LastName,
+            request.Password);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsError)
+        {
+            return Problem(result.Errors);
+        }
+
+        return Ok(result.Value);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    public async Task<IActionResult> LogIn(
+        LogInUserRequest request,
+        CancellationToken cancellationToken)
+    {
+        var command = new LogInUserCommand(request.Email, request.Password);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsError)
+        {
+            return Problem(result.Errors);
+        }
+
+        return Ok(result.Value);
+    }
+}
