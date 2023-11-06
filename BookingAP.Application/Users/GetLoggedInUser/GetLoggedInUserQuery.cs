5d38acdef188d441e0fbebf0e37a4fdb765c9c57
Sadek80 <mohamedsadk80@gmail.com>
2023-11-06 21:35:05 +0200

    Adds GetLoggedInUser UseCase

---
+++ BookingAP.Application/Users/GetLoggedInUser/GetLoggedInUserQuery.cs
@@ -0,0 +1,6 @@
+﻿using BookingAP.Application.Abstractions.Messaging;
+using ErrorOr;
+
+namespace BookingAP.Application.Users.GetLoggedInUser;
+
+public sealed record GetLoggedInUserQuery() : IQuery<ErrorOr<UserResponse>>;
---
+++ BookingAP.Application/Users/GetLoggedInUser/GetLoggedInUserQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using BookingAP.Application.Abstractions.Authentication;
+using BookingAP.Application.Abstractions.Data;
+using BookingAP.Application.Abstractions.Messaging;
+using Dapper;
+using ErrorOr;
+
+namespace BookingAP.Application.Users.GetLoggedInUser;
+
+internal sealed class GetLoggedInUserQueryHandler
+    : IQueryHandler<GetLoggedInUserQuery, ErrorOr<UserResponse>>
+{
+    private readonly ISqlConnectionFactory _sqlConnectionFactory;
+    private readonly IUserContext _userContext;
+
+    public GetLoggedInUserQueryHandler(
+        ISqlConnectionFactory sqlConnectionFactory,
+        IUserContext userContext)
+    {
+        _sqlConnectionFactory = sqlConnectionFactory;
+        _userContext = userContext;
+    }
+
+    public async Task<ErrorOr<UserResponse>> Handle(
+        GetLoggedInUserQuery request,
+        CancellationToken cancellationToken)
+    {
+        using var connection = _sqlConnectionFactory.CreateConnection();
+
+        const string sql = """
+            SELECT
+                "id" AS Id,
+                "name" AS Name,
+                "email" AS Email
+            FROM "users"
+            WHERE "IdentityId" = @IdentityId
+            """;
+
+        var user = await connection.QuerySingleAsync<UserResponse>(
+            sql,
+            new
+            {
+                _userContext.IdentityId
+            });
+
+        return user;
+    }
+}
