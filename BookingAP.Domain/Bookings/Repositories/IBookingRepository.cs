0e1296634c396d79b15e76916595b9dc2c9317c1
Sadek80 <mohamedsadk80@gmail.com>
2023-11-12 15:27:10 +0200

    Refactors Booking Response

--- BookingAP.Domain/Bookings/Repositories/IBookingRepository.cs
+++ BookingAP.Domain/Bookings/Repositories/IBookingRepository.cs
@@ -1,12 +1,13 @@
 ﻿using BookingAP.Domain.Abstractions;
 using BookingAP.Domain.Appartments;
+using BookingAP.Domain.Bookings.Abstractions;
 using BookingAP.Domain.Bookings.ValueObjects;
 
 namespace BookingAP.Domain.Bookings.Repositories
 {
     public interface IBookingRepository : IRepository<Booking, BookingId>
     {
-        Task<TResult?> GetBookingDetails<TResult>(BookingId bookingId, CancellationToken cancellationToken = default);
+        Task<TResult?> GetBookingDetails<TResult>(BookingId bookingId, CancellationToken cancellationToken = default) where TResult : IBookingResponse;
         Task<bool> IsOverlappingAsync(Appartment apartment,
                                       DateRange duration,
                                       CancellationToken cancellationToken = default);
